26e38bbf6ec9aa29dbbe5164ff2443fd23fdf51f
udithaabeykoon <udithaedu@gmail.com>
2020-06-14 02:54:21 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <button className="btn btn-primary">aaaa</button>
+      </div>
+    );
+  }
+}
+
+export default App;
